5f646f006f1921eeea450378b54eb3749bd21469
deepankur <deepankus@zeta.tech>
2022-06-14 09:32:08 +0530

    first commit

---
+++ src/main/kotlin/HelloWorld.kt
@@ -0,0 +1,11 @@
+class HelloWorld {
+
+    fun main(args: Array) {
+//        val user1 = User(name="Yogi", age=27)
+//        printUser(user1)
+    }
+}
+fun main(args: Array) {
+//    val user1 = User(name="Yogi", age=27)
+//    printUser(user1)
+}
