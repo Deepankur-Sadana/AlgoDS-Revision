034f2854891429925852a4233e4f8d337b8fb8e2
deepankur <deepankus@zeta.tech>
2022-06-19 16:47:52 +0530

    Merge Intervals, efficient/readable sol

--- src/main/kotlin/Merge Intervals.kt
+++ src/main/kotlin/Merge Intervals.kt
@@ -7,26 +7,25 @@ class `Merge Intervals` {
      *  [[1,3],[2,6],[8,10],[15,18]]
      */
     fun merge(intervals: Array<IntArray>): Array<IntArray> {
-        if (intervals.isEmpty()) return emptyArray()
         intervals.sortBy { it[0] }
         val res = ArrayList<IntArray>()
-        res.add(intervals[0])
-        for (i in 1 until intervals.size) {
-            val last = res.last()
-            val thisInterval = intervals[i]
-            if (canMerge(last, thisInterval)){
-                last[1] = Math.max(last[1], thisInterval[1])
-            } else {
+        for (element in intervals) {
+            val thisInterval = element
+            if (res.isEmpty()) {
                 res.add(thisInterval)
+                continue
+            } else {
+                val last = res.last()
+                if (last[1] >= thisInterval[0]) {
+                    last[1] = Math.max(last[1], thisInterval[1])
+                } else {
+                    res.add(thisInterval)
+                }
             }
         }
         return res.toTypedArray()
     }
 
-    private fun canMerge(last: IntArray, thisInterval: IntArray): Boolean {
-        if (last[1] >= thisInterval[0]) return true
-        return false
-    }
 
 }
 
